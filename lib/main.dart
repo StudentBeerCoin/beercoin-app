f4c6933fa6607b1e0eab1a46592b485e521d7400
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2022-01-01 00:47:01 +0100

    Removed default title

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,10 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  static const String _title = 'Flutter Code Sample';
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: _title,
+      title: "Beercoin",
       home: MyStatefulWidget(),
     );
   }
