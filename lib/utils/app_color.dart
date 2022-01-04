90c39061f0e3ff99ce896943de4e7af036aa1eea
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2022-01-04 10:43:00 +0100

    Application colors' library

---
+++ lib/utils/app_color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color primary = const Color(0xFFFFC83C);
+  static Color secondary = const Color(0xFFFFDC83);
+
+  static Color background = const Color(0xFFFFFFFF);
+  static Color gray = const Color(0xFFE4E4E4);
+  static Color black40 = const Color(0x66000000);
+  static Color headline = const Color(0xDE000000);
+  static Color black = const Color(0xFF000000);
+}
